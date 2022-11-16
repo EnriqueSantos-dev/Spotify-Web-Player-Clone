f8ab95c2e939cb18b1fe63c57b60f0c81bd66441
Enrique Santos <santosenrique869@gmail.com>
2022-11-16 17:09:57 -0300

    chore: delete spotify-web-api-node

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,9 @@
-import "../../styles/globals.css";
-import type { AppProps as NextAppProps } from "next/app";
-import { SessionProvider } from "next-auth/react";
 import { Session } from "next-auth";
+import { SessionProvider } from "next-auth/react";
+import type { AppProps as NextAppProps } from "next/app";
+import { ToastContainer } from "react-toastify";
+import "../../styles/globals.css";
+import "react-toastify/dist/ReactToastify.css";
 
 interface AppProps extends NextAppProps {
   session: Session;
@@ -10,6 +12,7 @@ interface AppProps extends NextAppProps {
 export default function App({ Component, pageProps, session }: AppProps) {
   return (
     <SessionProvider session={session}>
+      <ToastContainer limit={1} />
       <Component {...pageProps} />
     </SessionProvider>
   );
