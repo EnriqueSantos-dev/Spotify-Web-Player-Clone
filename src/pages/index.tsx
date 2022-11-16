f0bd3091e5e0ba24e92ccf5ee6795bd81c562425
Enrique Santos <santosenrique869@gmail.com>
2022-11-16 17:10:34 -0300

    feat: create index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,52 +1,70 @@
-import { GetServerSideProps } from "next";
+import React from "react";
+import { GetServerSideProps, NextPage } from "next";
 import { Session } from "next-auth";
-import { getSession, signOut } from "next-auth/react";
+import { getSession } from "next-auth/react";
 import Head from "next/head";
+import Center from "../components/Center";
+import Player from "../components/Player";
+import SideBar from "../components/SideBar";
+import useSongStore from "../stores/song";
+import useSpotifyAxios from "../hooks/useSpotifyAxios";
+import { Device } from "../interfaces/playlist-spotify";
+import { toast } from "react-toastify";
+import { AxiosError } from "axios";
 
-interface Props {
+interface HomeProps {
   session: Session;
 }
 
-export default function Home({ session }: Props) {
-  function handleLogout() {
-    signOut({ redirect: true });
+const HomePage: NextPage<HomeProps> = ({ session }) => {
+  const axiosApi = useSpotifyAxios();
+  const currentSong = useSongStore((state) => state.currentSong);
+
+  async function getDeviceActive() {
+    try {
+      const res = await axiosApi.get<{ devices: Device[] }>(
+        "me/player/devices"
+      );
+      const data = res.data?.devices;
+      const deviceActive = data?.some((device) => device.is_active);
+
+      if (!deviceActive) {
+        toast.error("Connect your device in app spotify.");
+      }
+    } catch (error: any) {
+      if (error instanceof AxiosError || error.reason === "NO_ACTIVE_DEVICE") {
+        toast.error("Connect your device in app spotify.");
+      }
+    }
   }
+
+  React.useEffect(() => {
+    getDeviceActive();
+  }, [session]);
 
   return (
     <div>
       <Head>
-        <title>Spotify Clone</title>
+        <link rel="icon" type="image/svg+xml" href="assets/favicon.svg" />
+        <link rel="icon" type="image/png" href="assets/favicon.png" />
+        <title>{currentSong?.name ?? "Spotify - Web Player Clone"}</title>
       </Head>
 
-      <main className="flex justify-center items-center h-screen w-screen bg-black">
-        <div className="flex flex-col text-white">
-          <h1 className="text-3xl font-semibold mb-4">
-            Hello, {session?.user.name}
-          </h1>
-
-          <button
-            className="bg-gray-700 text-white px-6 py-3 rounded font-semibold uppercase text-lg"
-            onClick={handleLogout}
-          >
-            SignOut
-          </button>
+      <main className="flex flex-col bg-black h-screen w-full overflow-hidden">
+        <div className="flex max-h-[calc(100vh_-_96px)] h-full">
+          <SideBar />
+          <Center userSession={session} />
+        </div>
+        <div className="stick bottom-0 min-h-[96px] z-10">
+          <Player />
         </div>
       </main>
     </div>
   );
-}
+};
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const session = await getSession(context);
-
-  if (!session) {
-    return {
-      redirect: {
-        destination: "/login",
-        permanent: false,
-      },
-    };
-  }
 
   return {
     props: {
@@ -54,3 +72,5 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
     },
   };
 };
+
+export default HomePage;
