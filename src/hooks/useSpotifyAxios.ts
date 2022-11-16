12a23e42e4baa4b5404eee9bdf526532d75ebb4b
Enrique Santos <santosenrique869@gmail.com>
2022-11-16 16:49:42 -0300

    feat: create hook useSpotifyAxios

---
+++ src/hooks/useSpotifyAxios.ts
@@ -0,0 +1,39 @@
+import React from "react";
+import { signIn, useSession } from "next-auth/react";
+import axios, { Axios, AxiosError } from "axios";
+
+const api = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_SPOTIFY_BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+function useSpotifyAxios() {
+  const { data: session } = useSession();
+
+  React.useEffect(() => {
+    if (session?.error === "RefreshAccessTokenError") {
+      signIn("spotify");
+      return;
+    }
+
+    if (
+      new Date(new Date().setMonth(new Date().getMonth() + 1)).toISOString() <
+      session?.expires!
+    ) {
+      signIn("spotify");
+      return;
+    }
+
+    if (session?.accessToken) {
+      api.defaults.headers.common[
+        "Authorization"
+      ] = `Bearer ${session?.accessToken}`;
+    }
+  }, [session]);
+
+  return api;
+}
+
+export default useSpotifyAxios;
